613548ea7b0226987e95b1ea8a2d3d98d6752c30
Zephni <zephni@hotmail.co.uk>
2022-06-06 10:41:18 +0100

    Removed all files ready for full installation files

--- app/Config/UserRunnableCommands.php
+++
@@ -1,5 +0,0 @@
-<?php
-    return [
-        'command:*',
-        'route:*'
-    ];
--- resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    Page content in /resources/views/home.blade.php
-@endsection
